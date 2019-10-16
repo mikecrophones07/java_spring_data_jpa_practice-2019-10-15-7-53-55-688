b1d0cc62c93bb2429c31eb4495e57b65749fb7fc
Mike Anthony A. Echon <Mikecrophones07>
2019-10-16 16:00:07 +0800

    [NAJIB&MIKE] Add update and delete functions

--- src/main/java/com/tw/apistackbase/controller/CompanyController.java
+++ src/main/java/com/tw/apistackbase/controller/CompanyController.java
@@ -5,17 +5,40 @@ import com.tw.apistackbase.repository.CompanyRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/companies")
 public class CompanyController {
 
     @Autowired
-    CompanyRepository repository;
+    private CompanyRepository repository;
 
     @GetMapping(produces = {"application/json"})
     public Iterable<Company> list() {
         return repository.findAll();
     }
+
+    @GetMapping(value = "/{name}", produces = {"application/json"})
+    public Company getCompanyByName(@PathVariable String name) {
+        return repository.findOneByName(name);
+    }
+
+    @PutMapping(produces = {"application/json"})
+    public Company updateCompanyInfo(@RequestBody Company company) {
+        Company company1 = repository.getOne(company.getId());
+        company1.setName(company.getName());
+        company1.setEmployees(company.getEmployees());
+        company1.setProfile(company.getProfile());
+        return repository.save(company1);
+    }
+
+    @DeleteMapping( value = "/{id}", produces = {"application/json"})
+    public List<Company> deleteCompanyInfo(@PathVariable Long id) {
+        repository.deleteById(id);
+        return repository.findAll();
+    }
+
 
     @PostMapping(produces = {"application/json"})
     public Company add(@RequestBody Company company) {
