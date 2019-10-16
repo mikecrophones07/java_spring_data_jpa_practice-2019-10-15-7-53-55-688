7309f9d941f5323bcfa8ace06c39a6eafbedf2d9
MIKE ANTHONY ECHON (SHP-IRIS-ISD-OOCLL/MNL) <ECHONMI@OOCL.COM>
2019-10-16 14:31:31 +0800

    edit CompanyController and Company Model and add repository for it to connect to JPA constant methods

--- src/main/java/com/tw/apistackbase/controller/CompanyController.java
+++ src/main/java/com/tw/apistackbase/controller/CompanyController.java
@@ -1,18 +1,24 @@
 package com.tw.apistackbase.controller;
 
 import com.tw.apistackbase.core.Company;
+import com.tw.apistackbase.repository.CompanyRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/companies")
 public class CompanyController {
+
+    @Autowired
+    CompanyRepository repository;
+
     @GetMapping(produces = {"application/json"})
     public Iterable<Company> list() {
-        return null;
+        return repository.findAll();
     }
 
     @PostMapping(produces = {"application/json"})
     public Company add(@RequestBody Company company) {
-        return null;
+        return repository.save(company);
     }
 }
